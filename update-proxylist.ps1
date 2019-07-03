a8c4b130174429a50e11feabe1fd323fdb7245af
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2019-07-02 23:03:51 -0400

    Initial script & sample CSV input.

---
+++ update-proxylist.ps1
@@ -0,0 +1,28 @@
+﻿# prep4article-download.ps1
+#
+
+$wd = 'C:\Support'
+$proxyListFileIn = "$wd\proxy-table.csv"
+$proxyListFileOut = "$wd\proxy-list-trimmed.txt"
+$testSite = "http://lynx.browser.org/"
+
+## trim proxy list down to working proxies
+# get list from http://spys.one/en/free-proxy-list/
+if (test-path $proxyListFileOut) { remove-item -Force $proxyListFileOut }
+import-csv $proxyListFileIn |% {
+  $ip = $_.'Proxy address:port'
+  $type = $($_.'Proxy type').tolower()
+  $proxy = "$($type)://$ip"
+  write-host "Testing proxy $proxy…"
+  $return = invoke-webrequest -UseBasicParsing -Uri $testSite -proxy $proxy
+  $worked = $? -and ($return -ne $null)
+  if (!($worked)) {
+    $return = invoke-webrequest -UseBasicParsing -Uri $testSite -proxy $proxy
+    $worked = $? -and ($return -ne $null)
+  }
+  if ($worked) {
+    write-host "Working proxy found."
+    $proxy | out-file -Append -Encoding ascii $proxyListFileOut
+  }
+}
+#>
